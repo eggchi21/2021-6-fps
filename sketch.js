1f59639886d177f2e3ee1519c06d4834b0399bbc
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2021-06-28 07:15:12 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -102,20 +102,68 @@ class Player {
     }
 }
 
+class Level {
+    constructor() {
+        this.walls = [];
+        this.tilemap = '';
+        this.tileSize = 35;
+        this.mapWidth = 0;
+        this.mapHeight = 0;
+    }
+    tileAt(x, y) {
+        return this.tilemap[this.mapWidth * y + x];
+    }
+    addWorldEdges() {
+        let s = this.tileSize;
+        let w = this.mapWidth;
+        let h = this.mapHeight;
+        this.walls.push(new Ray2(new Vec2(0, 0), new Vec2(s * w, 0)));
+        this.walls.push(new Ray2(new Vec2(0, 0), new Vec2(0, s * h)));
+        this.walls.push(new Ray2(new Vec2(s * w, s * h), new Vec2(-s * w, 0)));
+        this.walls.push(new Ray2(new Vec2(s * w, s * h), new Vec2(0, -s * h)));
+    }
+    /**
+     * @param {string} tilemap 
+     * @param {number} width 
+     * @param {number} height 
+     * @param {number} size 
+     */
+    addTilemap(tilemap, width, height, size) {
+        this.tilemap = tilemap;
+        this.mapWidth = width;
+        this.mapHeight = height;
+        this.tileSize = size;
+        let s = size;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let tile = this.tileAt(x, y);
+                if (tile === 'O' || tile === 'X') {
+                    this.walls.push(new Ray2(new Vec2(s * x, s * y), new Vec2(s, 0)));
+                    this.walls.push(new Ray2(new Vec2(s * x, s * y), new Vec2(0, s)));
+                    if (this.tileAt(x, y + 1) === '.') {
+                        this.walls.push(new Ray2(new Vec2(s * x, s * y + s), new Vec2(s, 0)));
+                    }
+                    if (this.tileAt(x + 1, y) === '.') {
+                        this.walls.push(new Ray2(new Vec2(s * x + s, s * y), new Vec2(0, s)));
+                    }
+                    if (tile === 'X') {
+                        this.walls.push(new Ray2(new Vec2(s * x, s * y), new Vec2(s, s)));
+                        this.walls.push(new Ray2(new Vec2(s * x + s, s * y), new Vec2(-s, s)));
+                    }
+                }
+            }
+        }
+    }
+}
+
 class Game {
     constructor() {
         this.player = new Player();
-        this.walls = [];
+        this.level = new Level();
     }
     reset() {
-        this.player.pos = new Vec2(150, 250);
-        this.player.angle = 0;
-
-        this.walls = [
-            Ray2.withPoints(new Vec2(50, 50), new Vec2(100, 300)),
-            Ray2.withPoints(new Vec2(100, 300), new Vec2(250, 200)),
-            Ray2.withPoints(new Vec2(250, 200), new Vec2(50, 50)),
-        ];
+        this.player.pos = new Vec2(118, 201);
+        this.player.angle = -PI / 2;
     }
 }
 
@@ -123,10 +171,29 @@ class Game {
 let game;
 
 function setup() {
-    createCanvas(720, 480);
+    createCanvas(640, 480);
 
     game = new Game();
     game.reset();
+
+    game.level.addTilemap(
+        (
+            '........' +
+            '........' +
+            '..OOO...' +
+            '..O.....' +
+            '........' +
+            '........' +
+            '........' +
+            '......O.' +
+            'OO...OO.' +
+            'OO...O..'
+        ),
+        8,
+        10,
+        35
+    );
+    game.level.addWorldEdges();
 }
 
 function draw() {
@@ -135,33 +202,33 @@ function draw() {
     // 背景
     background(64);
 
-    // 壁を描画. Draw the walls
-    strokeWeight(3);
-    stroke('white');
-    let walls = game.walls;
+    // 壁を描画. Draw walls of the level
+    strokeWeight(4);
+    stroke(224);
+    let walls = game.level.walls;
     for (let wall of walls) {
         line(wall.begin.x, wall.begin.y, wall.end.x, wall.end.y);
     }
 
     // プレイヤーを描画. Draw the player
-    stroke('yellow');
-    strokeWeight(20);
+    stroke(224, 224, 0);
+    strokeWeight(24);
     let player = game.player;
     point(player.pos.x, player.pos.y);
 
     // キー入力. Key input
-    if (keyIsDown(LEFT_ARROW)) player.angle -= PI / 120;
-    if (keyIsDown(RIGHT_ARROW)) player.angle += PI / 120;
+    if (keyIsDown(LEFT_ARROW)) player.angle -= PI / 180;
+    if (keyIsDown(RIGHT_ARROW)) player.angle += PI / 180;
 
-    // 3Dビューを描画. Draw the 3d view.
+    // 3Dビューを描画. Draw the 3dview.
     {
-        let viewRect = new Ray2(new Vec2(380, 40), new Vec2(320, 240));
+        let viewRect = new Ray2(new Vec2(305, 40), new Vec2(320, 240));
 
         let fov = PI / 2;
         let centerAngle = player.angle;
         let leftAngle = centerAngle - fov / 2;
         let rightAngle = centerAngle + fov / 2;
-        let beamTotal = 32;
+        let beamTotal = 40;
         let beamIndex = -1;
         for (let angle = leftAngle; angle < rightAngle - 0.01; angle += fov / beamTotal) {
             beamIndex++;
@@ -169,26 +236,26 @@ function draw() {
                 player.pos.copy(),
                 new Vec2(cos(angle), sin(angle)).mult(120)
             );
-            stroke('yellow');
-            strokeWeight(1);
-            line(beam.begin.x, beam.begin.y, beam.end.x, beam.end.y);
 
             // 光線が2枚以上の壁にあたっていたら、一番近いものを採用する。
             // Adapt the nearest beam.
             let allHitBeamWays = walls.map(wall => beam.intersection(wall))
                 .filter(pos => pos !== null)
                 .map(pos => pos.sub(beam.begin));
-            if (allHitBeamWays.length === 0) continue;
+            if (allHitBeamWays.length === 0) {
+                stroke(96, 96, 0);
+                strokeWeight(1);
+                line(beam.begin.x, beam.begin.y, beam.end.x, beam.end.y);
+                continue;
+            }
             let hitBeam = allHitBeamWays.reduce((a, b) => a.mag() < b.mag() ? a : b);
 
-            stroke('yellow');
-            strokeWeight(8);
+            // 3Dビューに縦線を1本描画する draw a line into 3dview
             let hitPos = hitBeam.add(beam.begin);
-            point(hitPos.x, hitPos.y);
-
             let wallDist = hitBeam.mag();
             let wallPerpDist = wallDist * cos(angle - centerAngle);
-            let lineHeight = constrain(2800 / wallPerpDist, 0, viewRect.way.y);
+            let lineHeight = constrain(3500 / wallPerpDist, 0, viewRect.way.y);
+            lineHeight -= lineHeight % 8;
             let lineBegin = viewRect.begin.add(
                 new Vec2(
                     viewRect.way.x / beamTotal * beamIndex,
@@ -196,15 +263,32 @@ function draw() {
                 )
             );
             let lightness = 224;
+            let lmft = 1.3; //lightness multiplier for topview
+            let tileSize = game.level.tileSize;
+            let pillarSize = 5;
+            if (
+                ((hitPos.x % tileSize < pillarSize) || (hitPos.x % tileSize > tileSize - pillarSize))
+                && ((hitPos.y % tileSize < pillarSize) || (hitPos.y % tileSize > tileSize - pillarSize))
+            ) {
+                stroke(215 * lmft, 179 * lmft, 111 * lmft); //wooden pillar color
+                fill(215, 179, 111);
+            } else {
+                stroke(lightness * lmft); //concrete wall color
+                fill(lightness);
+            }
             strokeWeight(0);
-            fill(lightness);
             rect(lineBegin.x, lineBegin.y, 7, lineHeight);
+
+            // ↑の縦線に対応した光線を、俯瞰図に描画する.
+            // draw a beam correspond to above 3dview line into the topview.
+            strokeWeight(1);
+            line(player.pos.x, player.pos.y, player.pos.add(hitBeam).x, player.pos.add(hitBeam).y);
         }
 
-        // 3Dビューの枠を描画. Draw border lines of the 3d view.
+        // 3Dビューの枠を描画. Draw border lines of the 3dview.
         noFill();
-        stroke('cyan');
-        strokeWeight(4);
+        stroke(66, 200, 251);
+        strokeWeight(6);
         rect(viewRect.pos.x, viewRect.pos.y, viewRect.way.x, viewRect.way.y);
     }
 }
@@ -214,3 +298,4 @@ function touchMoved(event) {
     player.pos.x = event.clientX;
     player.pos.y = event.clientY;
 }
+
